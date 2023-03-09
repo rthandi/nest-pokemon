3cfab7a692134249d158e7be1b080baabcce982c
rthandi <robbie.thandi@gmail.com>
2023-03-09 14:52:08 +0000

    feature: create pokemon table in db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonController } from './pokemon/pokemon.controller';
+import { PrismaModule } from './prisma/prisma.module';
+import { PokemonService } from './pokemon/pokemon.service';
 
 @Module({
   controllers: [AppController, PokemonController],
-  providers: [AppService],
+  providers: [AppService, PokemonService],
+  imports: [PrismaModule],
 })
 export class AppModule {}
