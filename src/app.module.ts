b5939d61efdb2f024f073718cc64eb27c19f9bfa
rthandi <robbie.thandi@gmail.com>
2023-03-09 15:03:34 +0000

    feature: update Pokemon module to fetch from db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PokemonController } from './pokemon/pokemon.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { PokemonService } from './pokemon/pokemon.service';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   controllers: [AppController, PokemonController],
   providers: [AppService, PokemonService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, PokemonModule],
 })
 export class AppModule {}
