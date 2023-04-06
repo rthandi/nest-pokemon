d7e630f8344643066a129a3dfb5ed3138c3d7f7e
rthandi <robbie.thandi@gmail.com>
2023-04-06 10:20:51 +0100

    feature: apply middleware to pokemon types endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonController } from './pokemon/pokemon.controller';
@@ -8,10 +8,15 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { TypeModule } from './type/type.module';
 import { TypeService } from './type/type.service';
 import { TypeController } from './type/type.controller';
+import { EndpointStatisticsMiddleware } from './middleware/endpointStatistics.middleware';
 
 @Module({
   controllers: [AppController, PokemonController, TypeController],
   providers: [AppService, PokemonService, TypeService],
   imports: [PrismaModule, PokemonModule, TypeModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(EndpointStatisticsMiddleware).forRoutes('pokemon/:type');
+  }
+}
--- src/middleware/endpointStatistics.middleware.ts
+++ src/middleware/endpointStatistics.middleware.ts
@@ -7,14 +7,16 @@ export class EndpointStatisticsMiddleware implements NestMiddleware {
   constructor(private prisma: PrismaService) {}
 
   use(req: Request, res: Response, next: NextFunction) {
-    this.prisma.endpointStatistics.create({
-      data: {
-        methodName: req.method,
-        path: req.path,
-        body: JSON.stringify(req.body),
-        args: JSON.stringify(req.params),
-      },
-    });
+    Promise.resolve(
+      this.prisma.endpointStatistics.create({
+        data: {
+          methodName: req.method,
+          path: req.path,
+          body: JSON.stringify(req.body),
+          args: JSON.stringify(req.params),
+        },
+      }),
+    );
 
     next();
   }
